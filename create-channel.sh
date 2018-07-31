a0b27e971c062b474a0bd98c25527526757f2e37
alacambra <alacambra@gmail.com>
2018-07-31 21:24:32 +0200

    added config files

---
+++ create-channel.sh
@@ -0,0 +1,10 @@
+#Required peer globals:
+#CORE_PEER_LOCALMSPID="Org1MSP"
+#CORE_PEER_TLS_ROOTCERT_FILE=crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+#CORE_PEER_MSPCONFIGPATH=crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+
+# Create genesys block:
+configtxgen -profile MultiOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+
+# Create channel configuration file:
+configtxgen -outputCreateChannelTx policies.tx -profile MultiOrgsChannel -channelID policies
