395b8357f81d6b1e4f8bf66f753593b861a5202c
alacambra <alacambra@gmail.com>
2018-09-04 20:40:47 +0200

    more changes

--- create-channel.sh
+++ create-channel.sh
@@ -3,8 +3,10 @@
 #CORE_PEER_TLS_ROOTCERT_FILE=crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 #CORE_PEER_MSPCONFIGPATH=crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 
+rm ./channel-artifacts/*
+
 # Create genesys block:
 configtxgen -profile MultiOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
 # Create channel configuration file:
-configtxgen -outputCreateChannelTx policies.tx -profile MultiOrgsChannel -channelID policies
+configtxgen -outputCreateChannelTx ./channel-artifacts/policies-channel.tx -profile MultiOrgsChannel -channelID policies-channel
---
+++ install-cc.sh
@@ -0,0 +1,9 @@
+CORE_PEER_ADDRESS=192.168.99.100:27051 CORE_PEER_LOCALMSPID="lacambraMSP" CORE_PEER_MSPCONFIGPATH=/Users/albertlacambra1/fabric-material/policies-check/crypto-config/peerOrganizations/lacambra.tech/users/Admin@lacambra.tech/msp/ CORE_PEER_TLS_ENABLED=false peer channel fetch 0 block.block -c policies-channel -o 192.168.99.100:7050
+
+CORE_PEER_ADDRESS=192.168.99.100:27051 CORE_PEER_LOCALMSPID="lacambraMSP" CORE_PEER_MSPCONFIGPATH=/Users/albertlacambra1/fabric-material/policies-check/crypto-config/peerOrganizations/lacambra.tech/users/Admin@lacambra.tech/msp/  CORE_PEER_TLS_ENABLED=false peer channel join -b block.block
+
+CORE_PEER_LOCALMSPID=lacambraMSP CORE_PEER_MSPCONFIGPATH=/Users/albertlacambra1/fabric-material/policies-check/crypto-config/peerOrganizations/lacambra.tech/users/Admin@lacambra.tech/msp/  CORE_PEER_ADDRESS=192.168.99.100:27051 CORE_LOGGING_PEER=info peer chaincode install -l java -n policies -p /Users/albertlacambra1/git/lacambra.tech/policies/polcies-trayning-cc/deployment -v 2
+
+CORE_PEER_LOCALMSPID=lacambraMSP CORE_PEER_MSPCONFIGPATH=/Users/albertlacambra1/fabric-material/policies-check/crypto-config/peerOrganizations/lacambra.tech/users/Admin@lacambra.tech/msp/  CORE_PEER_ADDRESS=192.168.99.100:27051 CORE_LOGGING_PEER=debug peer chaincode instantiate -n policies -v 1 -l java  -o 192.168.99.100:7050 -c '{"Args":["init"]}' -C policies-channel "OR   ('lacambraMSP.admin', 'lacambraMSP.peer')" 
+
+CORE_PEER_LOCALMSPID=lacambraMSP CORE_PEER_MSPCONFIGPATH=/Users/albertlacambra1/fabric-material/policies-check/crypto-config/peerOrganizations/lacambra.tech/users/Admin@lacambra.tech/msp/  CORE_PEER_ADDRESS=192.168.99.100:27051 CORE_LOGGING_PEER=debug peer chaincode upgrade -n policies -l java  -o 192.168.99.100:7050 -c '{"Args":["init"]}' -C policies-channel -P "OR   ('lacambraMSP.admin', 'lacambraMSP.peer')" -v 2
